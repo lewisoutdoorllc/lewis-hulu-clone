269df1723470c9c76f34658d670d882c6cd3ead9
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-11-21 09:44:08 -0500

    installed materail UI and icons; added the header.js and css files; set up things to get started

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.css'
+
+export default function Header() {
+    return (
+        <div>
+            <h1>This is the Header</h1>
+        </div>
+    )
+}
+
+
+
