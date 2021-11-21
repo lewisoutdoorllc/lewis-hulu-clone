40f6960862eb5d2b36d3e3c23dd90d29907c5f16
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-11-21 14:41:00 -0500

    set up nav bar for choosing show and or movie types; added styles UI and UX

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@
 import React from 'react'
 import './App.css';
 import Header from './Header'
+import Nav from './Nav'
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       {/* header componet section */}
       <Header />
       {/* navigation componet section */}
-
+      <Nav />
       {/* results componet section */}
     </div>
   );
